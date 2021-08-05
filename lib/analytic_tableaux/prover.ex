755ab1a35b044dfb76bd56b0f44c8e03e7f2cdac
Sam Frances <sam@samfrances.uk>
2021-08-06 00:03:54 +0100

    Record counterexample when argument shown to be invalid

--- lib/analytic_tableaux/prover.ex
+++ lib/analytic_tableaux/prover.ex
@@ -1,36 +1,37 @@
 defmodule AnalyticTableaux.Prover do
   defstruct(
     status: :unknown,
-    history: []
+    history: [],
+    counterexample: []
   )
   alias AnalyticTableaux.Node
 
   def prove(sequent = [_h|_t]) do
     starting_point = [Node.from_sequent(sequent)]
-    prove(starting_point, false, [starting_point])
+    prove(starting_point, false, nil, [starting_point])
   end
 
-  defp prove([], _found_counterexample? = false, history) do
+  defp prove([], _found_counterexample? = false, nil, history) do
     %__MODULE__{status: :valid, history: Enum.reverse(history)}
   end
 
-  defp prove(_queue, _found_counterexample? = true, history) do
-    %__MODULE__{status: :not_valid, history: Enum.reverse(history)}
+  defp prove(_queue, _found_counterexample? = true, counterexample, history) do
+    %__MODULE__{status: :not_valid, history: Enum.reverse(history), counterexample: counterexample}
   end
 
-  defp prove(queue, _found_counterexample? = false, history) do
-    {next_queue, found_counterexample?} = step(queue)
-    prove(next_queue, found_counterexample?, [next_queue|history])
+  defp prove(queue, _found_counterexample? = false, nil, history) do
+    {next_queue, found_counterexample?, counterexample} = step(queue)
+    prove(next_queue, found_counterexample?, counterexample, [next_queue|history])
   end
 
   def step([node|remaining]) do
     cond do
       Node.closed?(node) ->
-        {remaining, _found_counterexample? = false}
+        {remaining, _found_counterexample? = false, nil}
       Node.complete?(node) ->
-        {remaining, _found_counterexample? = true}
+        {remaining, _found_counterexample? = true, node}
       true ->
-        {Node.next(node) ++ remaining, _found_counterexample? = false}
+        {Node.next(node) ++ remaining, _found_counterexample? = false, nil}
     end
   end
 
