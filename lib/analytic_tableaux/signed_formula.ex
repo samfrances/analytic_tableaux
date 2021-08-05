ed5cce49ea971ef2ea699956a10c109d02216171
Sam Frances <sam@samfrances.uk>
2021-08-05 23:58:02 +0100

    Helpers for signed formulas

--- lib/analytic_tableaux/signed_formula.ex
+++ lib/analytic_tableaux/signed_formula.ex
@@ -1,3 +1,13 @@
 defmodule AnalyticTableaux.SignedFormula do
   defstruct formula: :nil, truth_value: true
+
+  alias AnalyticTableaux.Formula
+
+  def atomic?(%__MODULE__{formula: formula}) do
+    Formula.atomic?(formula)
+  end
+
+  def contradict(%__MODULE__{formula: formula, truth_value: truth_value}) do
+    %__MODULE__{formula: formula, truth_value: not truth_value}
+  end
 end
