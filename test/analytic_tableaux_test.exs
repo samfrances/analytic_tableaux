a580a8bbc19897704ee0951a6dddef132e027be2
Sam Frances <sam@samfrances.uk>
2021-08-07 10:02:42 +0100

    Tidy up after merge

--- test/analytic_tableaux_test.exs
+++ test/analytic_tableaux_test.exs
@@ -4,18 +4,16 @@ defmodule AnalyticTableauxTest do
   TestHelpers.SequentExamples.valid()
   |> Enum.each(fn sequent ->
     @tag sequent: sequent
-    @tag :pending
     test "The sequent #{sequent} is valid", context do
-      assert Prover.prove(context.sequent).status == :valid
+      assert AnalyticTableaux.prove(context.sequent).status == :valid
     end
   end)
 
   TestHelpers.SequentExamples.invalid()
   |> Enum.each(fn sequent ->
     @tag sequent: sequent
-    @tag :pending
     test "The sequent #{sequent} is NOT valid", context do
-      assert Prover.prove(context.sequent).status == :not_valid
+      assert AnalyticTableaux.prove(context.sequent).status == :not_valid
     end
   end)
 
